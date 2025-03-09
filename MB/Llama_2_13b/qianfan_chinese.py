4b7adfa654e5c4e3a59a4a62ec49cf6232b6615d
祁筱欣 <qixiaoxin@stu.sqxy.edu.cn>
2025-03-09 17:48:49 +0800

    feat: 新增Llama模型的中文支持
    - 新增MB/Llama-2-70B/qianfan_chinese.py
    - 新增MB/Llama_2_13b/qianfan_chinese.py
    - 新增MB/Llama_2_7b/qianfan_chinese.py

---
+++ MB/Llama_2_13b/qianfan_chinese.py
@@ -0,0 +1,19 @@
+"""
+这个模块提供了Llama-2-13b模型的交互接口
+"""
+
+from MZAPI.MB.Llama_2_13b.qianfan_chinese import Qianfan_Chinese
+
+
+def main() -> None:
+    """演示Llama-2-13b模型的使用"""
+
+    # 用户AK/SK在以下地址获取：
+    # https://console.bce.baidu.com/qianfan/ais/console/applicationConsole/application
+    model = Qianfan_Chinese("custom_client", "user_ak", "user_sk")
+    result = model.get_response("测试文本")
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
